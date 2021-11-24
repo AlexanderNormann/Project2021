2aa1f6c29eed4d7e7df5a7b0f394ec25a6bc0181
AlexanderNormann <alex654v@stud.kea.dk>
2021-11-24 12:06:29 +0100

    addede username

--- src/main/java/alphaproject/alphasolutionproject/domain/model/User.java
+++ src/main/java/alphaproject/alphasolutionproject/domain/model/User.java
@@ -3,4 +3,7 @@ package alphaproject.alphasolutionproject.domain.model;
 public class User {
 private String firstName;
 private String lastName;
+private String email;
+private String password;
+private int id;
 }
