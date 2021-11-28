32418a7ba086401961a7762d82092d38ff7976d4
jona826b <jona00k3@stud.kea.dk>
2021-11-28 20:29:06 +0100

    Oprettet html

--- src/main/java/alphaproject/alphasolutionproject/controllers/LoginController.java
+++ src/main/java/alphaproject/alphasolutionproject/controllers/LoginController.java
@@ -1,4 +1,14 @@
 package alphaproject.alphasolutionproject.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+
+    @GetMapping("/")
+    public String index(){ return "index"; }
+
+    @GetMapping("/create_account")
+    public String goToCreateUser(){ return "create_account"; }
 }
