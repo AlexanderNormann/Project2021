8be5620e639ffe7db68a126ceb7ff401bc7c40e3
AlexanderNormann <alex654v@stud.kea.dk>
2021-11-30 12:51:15 +0100

    Github merge fejl rettet

---
+++ src/main/java/alphaproject/alphasolutionproject/repositories/TaskRepository.java
@@ -0,0 +1,13 @@
+package alphaproject.alphasolutionproject.repositories;
+
+import alphaproject.alphasolutionproject.domain.model.Task;
+import alphaproject.alphasolutionproject.domain.model.User;
+import alphaproject.alphasolutionproject.domain.services.SampleExeption;
+
+import java.sql.SQLException;
+
+public interface TaskRepository {
+  Task createTask(Task task, User user) throws SampleExeption;
+  Task addTaskToProject(Task task) throws SQLException, SampleExeption;
+}
+
