8cbff492e2890de96717d1e33a157a6dd4d20d1f
Nicklas00 <nick553h@stud.kea.dk>
2021-11-30 11:18:24 +0100

    Man kan nu oprette et projekt når man er logget ind, og den bliver tilføjet til DB

---
+++ src/main/java/alphaproject/alphasolutionproject/controllers/ProjectController.java
@@ -0,0 +1,41 @@
+package alphaproject.alphasolutionproject.controllers;
+
+
+import alphaproject.alphasolutionproject.domain.model.Project;
+import alphaproject.alphasolutionproject.domain.model.User;
+import alphaproject.alphasolutionproject.domain.services.LoginService;
+import alphaproject.alphasolutionproject.domain.services.ProjectService;
+import alphaproject.alphasolutionproject.domain.services.SampleExeption;
+import alphaproject.alphasolutionproject.repositories.ProjectRepositoryImpl;
+import alphaproject.alphasolutionproject.repositories.UserRepositoryImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class ProjectController {
+  private ProjectService projectService = new ProjectService(new ProjectRepositoryImpl());
+
+  @GetMapping("/goToCreateProject")
+  public String createProject(Model model){
+    Project project = new Project();
+    model.addAttribute("project", project);
+    return "create_project";
+  }
+
+  @PostMapping("/saveProject")
+  public String saveProject(@ModelAttribute("project") Project project, HttpSession httpSession) throws SampleExeption {
+    User user = (User)httpSession.getAttribute("user");
+    projectService.createNewProject(project, user);
+    return "frontpage";
+  }
+
+
+
+
+
+}
